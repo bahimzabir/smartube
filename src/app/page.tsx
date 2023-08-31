83e9f666a685ab710d9f3c65d7e671e08282d37a
Abderrahim <bahimzabir@gmail.com>
2023-08-31 18:44:07 +0100

    navBar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import NavBar from '@/pages/navbar'
+import NavBar from '@/components/navbar'
 
 export default function Home() {
   return (
