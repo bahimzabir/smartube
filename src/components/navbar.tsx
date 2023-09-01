ed64fb07a1de400ff02c9522fae11aa9b9a964ae
Abderrahim <bahimzabir@gmail.com>
2023-09-01 01:34:26 +0100

    dark/light mode added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
-import { HiOutlineXCircle, HiOutlineMenuAlt2 } from 'react-icons/hi'
+import { HiOutlineXCircle, HiOutlineMenuAlt2, HiOutlineSun, HiOutlineMoon } from 'react-icons/hi'
+import { useTheme } from 'next-themes';
 
 const menu = [
     {
@@ -20,36 +21,42 @@ const menu = [
     },
 ]
 
-const NavBar: React.FC = () => {
+const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    let { theme, resolvedTheme, systemTheme, setTheme } = useTheme();
 
+    theme = resolvedTheme === 'system' ? systemTheme : resolvedTheme;
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
     return (
-        <nav className='bg-opacity-70'>
-            <div className='pl-4 pr-4 md:flex justify-between items-center'>
+        <nav className='sticky top-0 z-10 bg-black backdrop-filter backdrop-blur-lg bg-opacity-30'>
+            <div className=' pl-4 pr-4 md:flex justify-between items-center'>
                 <div className='flex justify-between p-4 md:p-6 '>
                     <div className='mr-0 md:mr-6 font-bold'>
-                        <a className='text-white hover:animate-pulse' href="#">
+                        <a className='hover:animate-pulse ' href="#">
                             Smart
                             <span className='text-red-600 hover:text-blue-500'> TUBE</span>
                         </a>
                     </div>
                     <div className='flex items-center'>
-                        <button onClick={toggleMenu} className='md:hidden text-white hover:animate-pulse' aria-label="Toggle Menu" >
-                            {isMenuOpen ? <HiOutlineXCircle color={"text-red-600"} /> : <HiOutlineMenuAlt2 />}
+                        <button onClick={toggleMenu} className='md:hidden  hover:animate-pulse' aria-label="Toggle Menu" >
+                            {isMenuOpen ? <HiOutlineXCircle color={"red"} size={30} /> : <HiOutlineMenuAlt2 size={30} color={"gray"} />}
                         </button>
                     </div>
                 </div>
                 <div className={`${isMenuOpen ? 'block' : 'hidden'} md:block`}>
                     <div className='grid justify-center md:flex m-2 p-4 space-x-2 md:space-x-3 font-semibold md:ml-4'>
                         {menu.map((item, index) => (
-                            <a className='text-white hover:animate-pulse ml-2 p-4 md:p-2' href={item.href} key={index}>
+                            <a className='hover:animate-pulse ml-2 p-4 md:p-2' href={item.href} key={index}>
                                 {item.name}
                             </a>
                         ))}
+                        <div className='cursor-pointer ml-3 pl-6 md:pl-0 p-4 md:p-2'>
+                            {theme === 'dark' ? <HiOutlineSun className='hover:animate-spin' size={30} color={"white"} onClick={() => { setTheme('light'); console.log(theme) }} /> :
+                                <HiOutlineMoon className='hover:animate-pulse' size={30} color={"black"} onClick={() => { setTheme('dark'); console.log(theme) }} />}
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/urlInput.tsx
@@ -0,0 +1,14 @@
+"use client"
+import {useEffect, useState} from "react";
+
+export default function UrlInput() {
+
+    return (
+        <main className="">
+            <div>
+                <input type="text" >
+                </input>
+            </div>
+        </main>
+    )
+}
